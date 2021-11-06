9d9e40aa6eb5431ab51c930a805a44cc95fa9455
Liam Osborn <liamosborn77@gmail.com>
2021-11-06 18:58:52 +0000

    Clamp position

--- Swap-Game/Assets/Scripts/Character Components/ClampPos.cs
+++ Swap-Game/Assets/Scripts/Character Components/ClampPos.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClampPos : MonoBehaviour
+namespace SwapGame.CharacterComponents
 {
-    private Vector2 screenBounds;
-    private Vector2 charSize;
-
-    void Start()
+    public class ClampPos : MonoBehaviour
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        charSize = GetComponent<BoxCollider2D>().size / 2;
-    }
+        private Vector2 screenBounds;
+        private Vector2 charSize;
 
-    void LateUpdate()
-    {
-        Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, -screenBounds.x + charSize.x, screenBounds.x - charSize.x);
-        viewPos.y = Mathf.Clamp(viewPos.y, -screenBounds.y + charSize.y, screenBounds.y - charSize.y);
-        transform.position = viewPos;
+        void Start()
+        {
+            screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+            charSize = GetComponent<BoxCollider2D>().size / 2;
+        }
+
+        void LateUpdate()
+        {
+            Vector3 viewPos = transform.position;
+            viewPos.x = Mathf.Clamp(viewPos.x, -screenBounds.x + charSize.x, screenBounds.x - charSize.x);
+            viewPos.y = Mathf.Clamp(viewPos.y, -screenBounds.y + charSize.y, screenBounds.y - charSize.y);
+            transform.position = viewPos;
+        }
     }
 }
