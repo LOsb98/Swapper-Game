82dfd8a855d022bf7ed8c80c22f941e5be0834b4
Liam Osborn <liamosborn77@gmail.com>
2021-11-08 13:18:21 +0000

    Extension methods

---
+++ Swap-Game/Assets/Scripts/Static Methods/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    /// <summary>
+    /// Convert a Vector3 to a Vector2 (For 2D: Z value will be zero)
+    /// </summary>
+    public static Vector3 ConvertToVector3(this Vector2 vector2)
+    {
+        float x = vector2.x;
+        float y = vector2.y;
+
+        Vector3 finalVector3 = new Vector3(x, y, 0f);
+
+        return finalVector3;
+    }
+}
